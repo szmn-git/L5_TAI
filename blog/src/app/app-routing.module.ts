a88657e31944aa07a34128c4fb57f0571b2f5679
Szymon Kieronski <informatyczne123@gmail.com>
2021-03-26 17:34:46 +0100

    100% (with finished navbar)

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BlogComponent} from "./components/blog/blog.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {HomeComponent} from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+
+  }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
